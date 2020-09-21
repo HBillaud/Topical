0b9f8a6f9ba0aeaa034bfa959db8152cb2ff45c9
HBillaud <h.billaud@att.net>
2020-09-21 17:48:24 -0400

    App can be successfully opened in web browser! Nodejs and hosting/deployment on Heroku work as expected

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-/*
-const hostname = '127.0.0.1';
-const port = 3000;
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end("Hello World");
+app.set('views', __dirname + "/views");
+
+// set the view engine to ejs
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index.html');
 });
 
-server.listen(port, hostname, () => {
-	console.log('Server running at http://${hostname}:${port}/');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
-**/
 
-console.log('Hello');
